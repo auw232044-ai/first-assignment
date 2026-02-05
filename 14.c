c24a1c0a486cf8f9098bcac57d841efad6b6f10a
auw232044-ai <auw232044@auw.edu.bd>
2026-02-05 22:18:18 +0600

    Add files via upload

---
+++ 14.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n = get_int("Enter n: ");
+    for (int i = 1; i <= n; i += 2)  
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
