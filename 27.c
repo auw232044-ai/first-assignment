49d00486d53c0554ac91cab76959970f7e6c73a3
auw232044-ai <auw232044@auw.edu.bd>
2026-02-05 22:24:36 +0600

    Add files via upload

---
+++ 27.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num = 1;
+
+    for (int i = 1; i <= 4; i++)
+    {
+        
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", num);
+            num++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
