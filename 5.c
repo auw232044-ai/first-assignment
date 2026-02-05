423daef8f0bc5a37317c723f80947b0260d34163
auw232044-ai <auw232044@auw.edu.bd>
2026-02-05 22:13:02 +0600

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n = get_int("Enter a number: ");
+
+    if (n > 0)
+    {
+        printf("Positive\n");
+    }
+    else if (n < 0)
+    {
+        printf("Negative\n");
+    }
+    else
+    {
+        printf("Zero\n");
+    }
+
+    return 0;
+}
